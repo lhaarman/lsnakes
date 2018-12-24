0382d04d76a29b9badcfff5d7562ffb859eeb84c
lhaarman <luukhaarman@gmail.com>
2018-12-24 16:08:06 +0100

    heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var http = require("http");
 var websocket = require("ws");
 var cookies = require("cookie-parser");
-
+// hope this worksS
 var indexRouter = require("./routes/index");
 var port = process.argv[2];
 var app = express();
